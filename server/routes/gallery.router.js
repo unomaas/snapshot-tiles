a6988f26179b9361eacf88ae4ce53fb1d5c8735a
Ryan Maas <rdmjobs@live.com>
2021-06-04 17:48:31 -0500

    Initial commit.  Installed depencies.  Added boostrap and pg.

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const galleryItems = require('../modules/gallery.data');
+const pool = require('../modules/pool.js');
 
 // DO NOT MODIFY THIS FILE FOR BASE MODE
 
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+//#region ⬇⬇ All document setup below:
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+//#endregion ⬆⬆ All document setup above. 
+
 
 function App() {
+    //#region ⬇⬇ All state variables & app load below:
+  // ⬇ State variables:
+  let [studentList, setStudentList] = useState([]);
+  // ⬇ On load, get guests:
+  useEffect(() => {
+    getStudent()
+  }, [])
+  //#endregion ⬆⬆ All state variables & app load above. 
+
     return (
       <div className="App">
         <header className="App-header">
