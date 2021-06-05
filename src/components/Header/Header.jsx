9273313082054642710ca24ab9840fd7aad75b2f
Ryan Maas <rdmjobs@live.com>
2021-06-04 19:50:01 -0500

    Added some header styling.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,14 @@
+import './Header.css';
+
 function Header() {
   return (
     <>
-      <header className="App-header">
-        <h1 className="App-title">Gallery of My Life</h1>
-      </header>
+      <div className="App-header">
+        <span>
+          <h1 className="App-header-title">Gallery of My Life App</h1>
+          <h3 className="App-header-subtitle"> v1.0</h3>
+        </span>
+      </div>
     </>
   ) // End return
 } // End Header
